4438ed8c376a1bf716e83801dc65fc35e9188162
Mark Santoro <mark_santoro@comcast.net>
2025-05-06 13:12:58 -0700

    DataController setup

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Mark Santoro on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
